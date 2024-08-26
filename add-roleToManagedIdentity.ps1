e69ef1af601d144fc7501235803189161eed3d21
Jos Lieben <jos@lieben.nu>
2024-08-26 20:07:39 +0200

    modernize

--- add-roleToManagedIdentity.ps1
+++ add-roleToManagedIdentity.ps1
@@ -10,12 +10,15 @@
     Updated: 27/08/2021
 #>
 Param(
-    [Parameter(Mandatory=$true)][String]$displayName="we-naima-aa",
-    [Parameter(Mandatory=$true)][String]$role="GroupMember.ReadWrite.All"
+    [Parameter(Mandatory=$true)][String]$displayName="LC-Runbooks",
+    [Parameter(Mandatory=$true)][String]$role="Exchange.ManageAsApp"
 )
-Connect-AzureAD 
-$Msi = (Get-AzureADServicePrincipal -Filter "displayName eq '$displayName'")
-Start-Sleep -Seconds 10
-$baseSPN = Get-AzureADServicePrincipal -Filter "appId eq '00000003-0000-0000-c000-000000000000'"
-$AppRole = $baseSPN.AppRoles | Where-Object {$_.Value -eq $role -and $_.AllowedMemberTypes -contains "Application"}
-New-AzureAdServiceAppRoleAssignment -ObjectId $Msi.ObjectId -PrincipalId $Msi.ObjectId -ResourceId $baseSPN.ObjectId -Id $AppRole.Id
+
+Connect-MgGraph -Scopes AppRoleAssignment.ReadWrite.All,Application.Read.All
+$MI = Get-MgServicePrincipal -Filter "DisplayName eq '$displayName'"
+$baseSPN = (Get-MgServicePrincipal -Filter "AppId eq '00000003-0000-0000-c000-000000000000'") #use 00000002-0000-0ff1-ce00-000000000000 for Exchange Online
+foreach($appRole in $baseSPN.AppRoles){
+    if($appRole.Value -eq $role){
+        New-MgServicePrincipalAppRoleAssignment -ServicePrincipalId $MI.Id -PrincipalId $MI.Id -AppRoleId $appRole.Id -ResourceId $baseSPN.Id
+    }
+}
