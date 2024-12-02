82aa331260157dd962a788e0870b60e4b29fcc40
Jos Lieben <jos@lieben.nu>
2024-12-02 14:37:08 +0100

    enhance principal type retrieval

--- M365Permissions/private/get-EntraGroupMembers.ps1
+++ M365Permissions/private/get-EntraGroupMembers.ps1
@@ -9,5 +9,16 @@ function Get-EntraGroupMembers {
     )
 
     $groupMembers = new-GraphQuery -Method GET -Uri "https://graph.microsoft.com/v1.0/groups/$groupId/transitiveMembers" | Where-Object {$_ -and $_."@odata.type" -ne "#microsoft.graph.group" }
+    for($i=0;$i -lt $groupMembers.count;$i++){
+        if($groupmembers[$i]."@odata.type" -eq "#microsoft.graph.user"){
+            if($groupMembers[$i].userPrincipalName -like "*#EXT#@*"){
+                $groupMembers[$i] | Add-Member -MemberType NoteProperty -Name "principalType" -Value "External User" -Force
+            }else{
+                $groupMembers[$i] | Add-Member -MemberType NoteProperty -Name "principalType" -Value "Internal User" -Force
+            }
+        }else{
+            $groupMembers[$i] | Add-Member -MemberType NoteProperty -Name "principalType" -Value $groupmembers[$i]."@odata.type".Split(".")[2] -Force
+        }
+    }
     return $groupMembers
 }
