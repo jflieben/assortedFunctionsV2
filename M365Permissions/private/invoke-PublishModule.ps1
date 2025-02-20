bf326ae355df7bb6a77af522dffd245f54f30870
Jos Lieben <jos@lieben.nu>
2025-02-20 15:51:20 +0100

    move m365permissions to its own project

--- M365Permissions/private/invoke-PublishModule.ps1
+++
@@ -1,9 +0,0 @@
-<#
-    Author               = "Jos Lieben (jos@lieben.nu)"
-    CompanyName          = "Lieben Consultancy"
-    Copyright            = "https://www.lieben.nu/liebensraum/commercial-use/"
-#>    
-function invoke-PublishModule {
-    $apiKey = ""
-    Publish-Module -Path "C:\git\assortedFunctionsV2\M365Permissions" -NuGetApiKey $apiKey -Verbose
-}
